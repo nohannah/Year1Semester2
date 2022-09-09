8007066bcc15d4e2cbd728f9a424cab4aa9e8bd3
nohannah <67176677+nohannah@users.noreply.github.com>
2022-09-09 14:41:02 +0700

    09.09

---
+++ lesson.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int var = 1 ;
+    cout <<"Value of the variable is :"<< var <<endl;
+    cout << "Memory address of the variable is :" <<&var;
+}
